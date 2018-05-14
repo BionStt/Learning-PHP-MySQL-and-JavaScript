62375b8f36ec40ed1f4bca31be04be0a44d4baae
Shane Sexton <redhatshane@protonmail.com>
2018-05-13 17:15:14 -0700

    Added basic PHP stuff

---
+++ Basic-PHP/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// This PHP file contains basic syntax practice
+
+// Simple variable assignment and output
+  $userName = "Hugh Thompson Jr.";
+  echo "The username is ";
+  echo $userName;
+  echo "<br><br>";	
+
+// Simple arithmetic
+  $birthYear = 1943;
+  $currentYear = 2018;
+  $age = $currentYear - $birthYear;
+  echo $userName;
+  echo " would be ";
+  echo $age;
+  echo " this year.<br><br>";
+
+// Simple array
+
+  $crewNames = array('Hugh Thompson Jr.','Larry Colburn','Glenn Andreotta');
+  echo $crewNames[0];
+  echo " had two crewmates: ";
+  echo $crewNames[1];
+  echo " and ";
+  echo $crewNames[2];
+  echo ". The crew intervened during the My Lai massacre in Vietnam.<br><br>"
+	
+?>
