8d929f135725f84369bcc67194039ef0018aa09a
Shane Sexton <redhatshane@protonmail.com>
2018-05-20 15:29:15 -0700

    added new page for functions testing

---
+++ BasicPHP/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+// Simple string functions
+
+  echo strrev("edcbA") . "<br>";
+  echo str_repeat("Wacka ",2) . "<br>";
+  echo strtoupper("uppercase") . "<br>";
+
+
+// Simplest of functions
+
+function output($n1)
+  {
+    echo $n1;
+  }
+
+output("Test.... <br>");
+?>
