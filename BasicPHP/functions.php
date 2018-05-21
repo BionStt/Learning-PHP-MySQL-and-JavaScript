a063e54dfd6fba9ad6a49bf6142c0852c2fccb27
Shane Sexton <redhatshane@protonmail.com>
2018-05-20 18:02:44 -0700

    playing with functions

--- BasicPHP/functions.php
+++ BasicPHP/functions.php
@@ -15,4 +15,40 @@ function output($n1)
   }
 
 output("Test.... <br>");
-?>
+
+// Nested functions
+
+output(strtoupper("testing.....") . " <br><br>");
+
+// Reverse Sign
+
+function reverseSign ($int1)
+{
+  if ($int1 != 0)
+  {
+  $int1 *= -1;
+  }
+  elseif ($int1 == 0)
+  {
+  return "Zero doesn't have a sign.";
+  }
+  return $int1;
+}
+
+// Test variables
+  
+$test1 = 15;
+$test2 = -4;
+$test3 = 0;
+
+// Format output
+
+function wrapper($int1)
+{
+  return $int1 . " ==> " . reverseSign($int1) . "<br>";
+}
+
+
+echo wrapper($test1);
+echo wrapper($test2);
+echo wrapper($test3);
