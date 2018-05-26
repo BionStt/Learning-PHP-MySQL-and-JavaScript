b15cc9c9ddf16cf11ee40216240f0681dde09656
Shane Sexton <redhatshane@protonmail.com>
2018-05-26 13:36:02 -0700

    Working with arrays, yo

--- BasicPHP/arrays.php
+++ BasicPHP/arrays.php
@@ -23,7 +23,66 @@ for ($i = 0 ; $i < 3 ; $i++)
   $evil['Cosby']   = TRUE;
 
 echo "<br>";
+
+// Using array() and foreach
  
 print_r($evil);
+$numbers = array(2,4,8);
+
+echo "<br>";
+print_r($numbers);
+
+echo "<br>";
+foreach($numbers as $item)
+  {
+  echo $item * 2 . "<br>";
+  }
+
+//is_array()
+
+echo "<br>" . (is_array($evil)) ? "Evil is an array" : "Evil is not an array";
+
+//count()
+
+echo "<br> Count:" . count($evil);
+
+//sort()
+
+echo "<br>";
+sort($evil, SORT_NUMERIC);
+print_r($evil);
+
+//shuffle()
+
+$breed = array("Maine Coon", "Calico", "Tortie", "American Shorthair", "Norwegian Forest Cat");
+echo "<br><br>";
+foreach($breed as $q)
+  {
+  echo "$q<br>";
+  }
+shuffle($breed);
+echo "<br>";
+foreach($breed as $q)
+  {
+  echo "$q<br>";
+  }
+
+//explode()
+
+echo "<br>";
+$fullName = "Barack H. Obama";
+$temp = explode(' ',$fullName);
+print_r($temp);
+
+//compact()
+
+echo "<br><br>";
+$word1 = "I";
+$word2 = "like";
+$word3 = "cats";
+
+$sentence = compact('word1', 'word2', 'word3');
+
+print_r($sentence);
 
 ?>
